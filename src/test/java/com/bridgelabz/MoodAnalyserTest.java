8628ba79da9ef41a463981cc18292f9c0e521b8a
Utkarsh Mishra <utkarshkr945@gmail.com>
2021-10-06 22:20:52 +0530

    Added: Given Null Mood Should Return Happy

--- src/test/java/com/bridgelabz/MoodAnalyserTest.java
+++ src/test/java/com/bridgelabz/MoodAnalyserTest.java
@@ -19,9 +19,9 @@ public class MoodAnalyserTest {
     }
 
     @Test
-    public void givenMessage_Null_ShouldReturnExceptionHandled() {
+    public void givenMessage_Null_ShouldReturnHappy() {
         MoodAnalyser moodanalyser = new MoodAnalyser(null);
         String actualResult = moodanalyser.analyseMood();
-        Assert.assertEquals("Exception Handled", actualResult);
+        Assert.assertEquals("Happy", actualResult);
     }
 }
