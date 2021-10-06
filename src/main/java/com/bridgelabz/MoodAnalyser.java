fad7e5d723a5a264ad7206f33ce86fd8635953dd
Utkarsh Mishra <utkarshkr945@gmail.com>
2021-10-06 21:25:33 +0530

    Added: Welcome Message

---
+++ src/main/java/com/bridgelabz/MoodAnalyser.java
@@ -0,0 +1,18 @@
+package com.bridgelabz;
+
+/**
+ * Purpose  - Welcome Message
+ * @author  - Utkarsh Mishra
+ * @version - 16.0
+ * @since   - 2021-10-05
+ */
+
+public class MoodAnalyser {
+    static void welcome(){
+        System.out.println("Welcome to Mood Analyzer Problem");
+    }
+
+    public static void main(String[] args) {
+    welcome();
+    }
+}
