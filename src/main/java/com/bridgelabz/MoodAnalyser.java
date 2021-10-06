c5b4aeebb39ce1a8a6d568c41d3b74a2e1f466b4
Utkarsh Mishra <utkarshkr945@gmail.com>
2021-10-06 23:09:01 +0530

    Added: Given NULL Mood Should Throw MoodAnalysisException

--- src/main/java/com/bridgelabz/MoodAnalyser.java
+++ src/main/java/com/bridgelabz/MoodAnalyser.java
@@ -10,8 +10,11 @@ package com.bridgelabz;
 public class MoodAnalyser {
     public static String message;
 
+    public MoodAnalyser() {
+    }
+
     // Constructor
-    public MoodAnalyser(String message) {
+    public MoodAnalyser(String message) throws MoodAnalysisException {
         this.message = message;
         analyseMood();
     }
@@ -26,7 +29,7 @@ public class MoodAnalyser {
         this.message = message;
     }
 
-    public static String analyseMood() {
+    public static String analyseMood() throws MoodAnalysisException {
         try {
             if (message.toLowerCase().contains("sad")) {
                 return "Sad";
@@ -34,7 +37,7 @@ public class MoodAnalyser {
                 return "Happy";
             }
         } catch (NullPointerException e) {
-            return "Happy";
+            throw new MoodAnalysisException("Entered Invalid mood");
         }
     }
 }
