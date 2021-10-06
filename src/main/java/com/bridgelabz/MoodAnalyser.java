29c2ba7f53122d194230fb578997edce881be02e
Utkarsh Mishra <utkarshkr945@gmail.com>
2021-10-06 21:32:00 +0530

    Added: “I am in Sad Mood” message Should Return SAD

--- src/main/java/com/bridgelabz/MoodAnalyser.java
+++ src/main/java/com/bridgelabz/MoodAnalyser.java
@@ -1,18 +1,16 @@
 package com.bridgelabz;
 
 /**
- * Purpose  - Welcome Message
+ * Purpose  - “I am in Sad Mood” message Should Return SAD
  * @author  - Utkarsh Mishra
  * @version - 16.0
  * @since   - 2021-10-05
  */
 
 public class MoodAnalyser {
-    static void welcome(){
-        System.out.println("Welcome to Mood Analyzer Problem");
-    }
-
-    public static void main(String[] args) {
-    welcome();
+    public static String analyseMood(String message) {
+        if (message.toLowerCase().contains("sad")) {
+        }
+        return "Sad";
     }
 }
---
+++ src/test/java/com/bridgelabz/MoodAnalyserTest.java
@@ -0,0 +1,14 @@
+package com.bridgelabz;
+import org.junit.Test;
+import org.junit.Assert;
+
+public class MoodAnalyserTest
+{
+    // Test case to analyze sad mood of the user
+    @Test
+    public void givenMessage_WhenProper_ShouldReturnSad() {
+        MoodAnalyser moodanalyser=new MoodAnalyser();
+        String actualResult=moodanalyser.analyseMood("User is Sad");
+        Assert.assertEquals("Sad",actualResult);
+    }
+}
